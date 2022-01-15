75ba01be0377fe1ac93de99072d7f8dc2412045e
z9fr <z9fr@pm.me>
2022-01-16 01:34:36 +0530

    fixed issues with sending cbor in wrong way.

--- src/codectrl.js
+++ src/codectrl.js
@@ -1,5 +1,13 @@
 const StackTrace = require("stacktrace-js");
-const ReadFile = require("./ReadFile");
+const cbor = require("cbor");
+const assert = require("assert");
+const socket = require("net").Socket();
+const fs = require("fs");
+
+let encoded = cbor.encode(true);
+cbor.decodeFirst(encoded, (error, obj) => {
+  assert.ok(obj === true);
+});
 
 module.exports.log = async function (...args) {
   let appHost = "127.0.0.1";
@@ -28,24 +36,34 @@ module.exports.log = async function (...args) {
     let firstLine = LineNumber;
     let lastLine = LineNumber + appSurround;
 
+    /*
+    check if the lineNumber if greater than the number user requested if so we will calculate the first Line based on that
+    */
+
     if (LineNumber >= appSurround) {
       firstLine = LineNumber - appSurround;
     }
     code_lines = {};
 
-    for (let i = firstLine; i <= lastLine; i++) {
-      let file_line = ReadFile.ReadFileLines(stackframes[1].fileName, i);
-      code_lines[i] = file_line;
+    for (let i = firstLine; i < lastLine; i++) {
+      let file_line = ReadFileLines(stackframes[1].fileName, i);
+      code_lines[i] = file_line.toString();
     }
+    const finalstack = buildStack(stackframes[1]);
 
-    getJson(
-      (message = messageBody),
-      (stack = stackframes[1]),
-      (message_type = messageBody_type),
-      (lineNumber = LineNumber),
-      (file_name = stackframes[1].fileName),
-      (code_snippet = code_lines)
-    );
+    const obj = {
+      message: messageBody,
+      message_type: messageBody_type,
+      line_number: LineNumber,
+      code_snippet: code_lines,
+      file_name: stackframes[1].fileName,
+      stack: [finalstack],
+      warnings: [],
+      address: "127.0.0.1",
+    };
+
+    encoded = cbor.encode(obj);
+    sendData(encoded, appPort);
   };
 
   const errback = function (err) {
@@ -55,15 +73,28 @@ module.exports.log = async function (...args) {
   StackTrace.get().then(callback).catch(errback);
 };
 
-const getJson = (...args) => {
-  console.log({
-    message: Object.values({ message }).toString(),
-    message_type: Object.values({ message_type }).toString(),
-    line_number: parseInt(Object.values({ lineNumber })),
-    code_snippet: Object.values({ code_snippet }),
-    file_name: Object.values({ file_name }).toString(),
-    stack: Object.values({ stack }),
-    warnings: [],
-    address: "127.0.0.1",
-  });
+const ReadFileLines = (filename, line_no) => {
+  const data = fs.readFileSync(filename, "utf8");
+  const lines = data.split("\n");
+
+  if (+line_no > lines.length) {
+    return "";
+  }
+  return lines[+line_no];
+};
+
+const buildStack = (stackTrace) => {
+  return {
+    name: stackTrace.functionName,
+    code: stackTrace.source,
+    file_path: stackTrace.fileName,
+    line_number: stackTrace.lineNumber,
+    column_number: stackTrace.columnNumber,
+  };
+};
+
+const sendData = async (data, port) => {
+  socket.connect(port);
+  socket.write(data);
+  socket.end();
 };
--- src/tests.js
+++ src/tests.js
@@ -4,10 +4,18 @@ const codectrlLogtest = () => {
   codectrl.log(
     (host = "localhost"),
     (port = 3001),
-    (surround = 4),
+    (surround = 2),
     "messaet to display",
     "test"
   );
 };
 
 codectrlLogtest();
+
+codectrl.log(
+  (host = "localhost"),
+  (port = 3001),
+  (surround = 3),
+  "messaet to display",
+  "test"
+);
